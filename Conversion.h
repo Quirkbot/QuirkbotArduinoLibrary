b8972a30f488d774bc4955388c5a6519c9cfad92
Paulo Barcelos <paulo@azucrina.org>
2014-07-03 22:19:59 +0200

    Renamed range to conversion

---
+++ Conversion.h
@@ -0,0 +1,58 @@
+#ifndef Conversion_h_
+#define Conversion_h_
+
+#include "Arduino.h"
+
+#include "Bot.h"
+#include "Node.h"
+#include "Input.h"
+#include "Output.h"
+#include "ContainsInputs.h"
+#include "Streams.h"
+
+class Conversion :
+public Node,
+public Contains4Inputs<float, float, float, float>,
+public InputOutputStream<float>
+{
+	public:
+	
+	Conversion():
+	Contains4Inputs<float, float, float, float>
+		(inputMin, inputMax, outputMin, outputMax),
+	InputOutputStream<float>
+		(source, value){
+		registerInput(source);
+		registerInput(inputMin);
+		registerInput(inputMax);
+		registerInput(outputMin);
+		registerInput(outputMax);
+		
+		source = 0.0;
+		inputMin = 0.0;
+		inputMax = 1.0;
+		outputMin = 0.0;
+		outputMax = 1.0;
+	};
+
+	Input<float> source;
+	
+	Input<float> inputMin;
+	Input<float> inputMax;
+	Input<float> outputMin;
+	Input<float> outputMax;
+
+	Output<float> value;
+
+	protected:
+
+	void onInternalInputChange(BaseInput &input);
+};
+
+void Conversion::onInternalInputChange(BaseInput &input){
+	if(&input == &source){
+		value = mapFloat(source, inputMin, inputMax, outputMin, outputMax);
+	}
+};
+
+#endif
