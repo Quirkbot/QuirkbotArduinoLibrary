31332a4ef32a133ee15149e7c5c7bcb850442149
Paulo Barcelos <paulo@azucrina.org>
2015-03-05 14:50:05 +0100

    Now servo has internal stall controll

--- ServoMotor.h
+++ ServoMotor.h
@@ -7,16 +7,24 @@
 
 class ServoMotor:
 public Node,
+public HasInterval,
 public HasIn<float>{
 	public:
 	
 	ServoMotor():
+	HasInterval
+		(this),
 	HasIn<float>
 		(this){
-
 		registerInput(pin);
 
+		interval = 0.5;
+
+		attached = false;
 		angle = -1;		
+		iddleAngle = -1;
+		iddleCount = 0;	
+		iddleMaxTime = 3;			
 	};
 
 	Input<int> pin;
@@ -24,20 +32,69 @@ public HasIn<float>{
 	protected:
 
 	void onInternalInputChange(BaseInput &internalInput);
+	void onInterval();
+
+	void attach();
+	void detach();
+	void write();
 
 	_libs_Servo servo;
 	int angle;
+	
+	int attachedPin;
+	bool attached;
+	int iddleAngle;
+	int iddleCount;
+	int iddleMaxTime;
 };
 void ServoMotor::onInternalInputChange(BaseInput &internalInput){
+
 	if(&internalInput == &pin){
-		servo.attach(pin.get());
+		if(!attached){
+			attach();
+		}
+		else{
+			attach();
+			write();
+		}
 	}
 	else if(&internalInput == &in){
-		int newAngle = in.get() * 180.0;
-		if(newAngle != angle){
-			angle = newAngle;
-			servo.write(angle);
-		}		
+		if(!attached){
+			attach();
+		}
+		write();		
+	}
+}
+void ServoMotor::attach(){
+	if(attached) detach();
+
+	attached = true;
+	attachedPin = pin.get();
+	servo.attach(attachedPin);
+}
+void ServoMotor::detach(){
+	attached = false;
+	servo.detach();
+}
+void ServoMotor::write(){
+	if(in.get() == -1) return;
+
+	int newAngle = in.get() * 180.0;
+	if(newAngle != angle){
+		angle = newAngle;
+		servo.write(angle);
+	}
+}
+void ServoMotor::onInterval(){
+	if(!attached) return;
+
+	if(iddleAngle == angle) iddleCount++;
+	else iddleCount = 0;
+
+	iddleAngle = angle;
+
+	if(iddleCount >= iddleMaxTime) {
+		detach();
 	}
 }
 
