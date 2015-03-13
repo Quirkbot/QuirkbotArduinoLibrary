ed69340eca62d7fcbd95010e4e555909dc4cb0dc
Paulo Barcelos <paulo@azucrina.org>
2015-03-13 12:08:17 +0100

    Added filter helpers

---
+++ Filters.h
@@ -0,0 +1,90 @@
+#ifndef Filters_h_
+#define Filters_h_
+
+#define QB_NO_FILTER 0
+#define QB_MEDIAN_FILTER 1
+#define QB_LOW_PASS_FILTER 2
+
+class FilterBase {
+	public:
+
+	virtual FilterBase();
+	virtual void push(float number){};
+	
+	float get(){
+		return value;
+	} 
+
+	protected:
+
+	float value;
+	float alpha;
+	float beta;
+	float gamma;
+};
+
+class NoFilter :
+public FilterBase 
+{
+	public:
+	void push(float number){
+		value = number;
+	}
+};
+
+class MedianFilter :
+public FilterBase 
+{
+	public:
+	
+	MedianFilter(){
+		a = 0;
+		b = 0;
+		c = 0;
+		value = 0;
+	}
+
+	void push(float number){
+		a = b;
+		b = c;
+		c = number;
+
+		if (c < b) {
+			if (c < a) {
+				if (b < a) value = b;
+				else value = a;
+			}
+			else value = c;
+		}
+		else {
+			if (c < a) value = c;
+			else {
+				if (b < a) value = a;
+				else value = b;
+			}
+		}
+	}
+
+	private:
+
+	float a;
+	float b;
+	float c;
+};
+
+class LowPassFilter :
+public FilterBase 
+{
+	public:
+	
+	LowPassFilter(){
+		value = 0;
+		alpha = 0.5;
+	}
+
+	void push(float number){
+		value = value * alpha + number * (1 - alpha);
+	}
+};
+
+#endif
