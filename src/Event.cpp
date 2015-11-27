df53a82fb0c1ab9a99916251578792a709eb6a45
Paulo Barcelos <paulo@azucrina.org>
2015-11-27 08:27:06 +0100

    Fixed critical bug in event listeners

--- src/Event.cpp
+++ src/Event.cpp
@@ -3,20 +3,22 @@
 Event::Event(){}
 Event::~Event(){}
 void Event::add(Input* input, TypedInputHandler handler){
-	int pos = position(handler);
+	int pos = position(input, handler);
 	if(pos != -1) return;
 	typedInputs.push(input);
 	typedInputHandlers.push(handler);
 }
-void Event::remove(TypedInputHandler handler){
-	int pos = position(handler);
+void Event::remove(Input* input, TypedInputHandler handler){
+	int pos = position(input, handler);
 	if(pos == -1) return;
 	typedInputs.erase(pos);
 	typedInputHandlers.erase(pos);
 }
-int Event::position(TypedInputHandler handler){
+int Event::position(Input* input, TypedInputHandler handler){
 	for(int i=0; i<typedInputHandlers.size(); i++){
-		if(typedInputHandlers[i] == handler) return i;
+		if(typedInputs[i] == input && typedInputHandlers[i] == handler) {
+			return i;
+		}
 	}
 	return -1;
 }
