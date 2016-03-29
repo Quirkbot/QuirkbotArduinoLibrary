246a93ae9bf032373ee9b67a4105d3586a1cf900
Paulo Barcelos <paulo@azucrina.org>
2016-03-29 08:41:04 +0200

    Added sensitivity

--- src/CircuitTouch.h
+++ src/CircuitTouch.h
@@ -3,8 +3,8 @@
 
 #include "CommonNodeIncludes.h"
 
-#define QB_MAKEY_TOUCH_MAX_TIME 10000
-#define QB_MAKEY_TOUCH_DISCHARGE_TIME 1000
+#define QB_CIRCUIT_TOUCH_MAX_TIME 10000
+#define QB_CIRCUIT_TOUCH_DISCHARGE_TIME 1000
 
 class CircuitTouch :
 public Node,
@@ -19,6 +19,7 @@ public HasOut
 	Input place;
 	Input min;
 	Input max;
+	Input sensitivity;
 
 	void update();
 
