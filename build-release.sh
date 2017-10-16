f94a0213ba16c1cfddcdaffe5de37998098100d6
Paulo Barcelos <paulo@azucrina.org>
2017-10-16 16:03:57 +0200

    Deploy files

--- build-release.sh
+++ build-release.sh
@@ -2,17 +2,17 @@ temp=$(mktemp -dt "$0")
 src=$(pwd)
 
 rm Quirkbot.zip
-cp -r ./* $temp/
+cp -rf ./* $temp/
 
-rm -r $temp/.git
-rm -r $temp/.gitignore
-rm -r $temp/.npmignore
-rm -r $temp/node_modules
-rm -r $temp/gulpfile.js
-rm -r $temp/library.template.properties
-rm -r $temp/build-release.sh
-rm -r $temp/s3_publish
-rm -r $temp/s3-publish.sh
+rm -rf $temp/.git
+rm -rf $temp/.gitignore
+rm -rf $temp/.npmignore
+rm -rf $temp/node_modules
+rm -rf $temp/gulpfile.js
+rm -rf $temp/library.template.properties
+rm -rf $temp/build-release.sh
+rm -rf $temp/s3_publish
+rm -rf $temp/s3-publish.sh
 
 cd $temp
 zip -vr Quirkbot.zip \
